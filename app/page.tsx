7dbd21d25c6fe4797da55e17f13dd6868535e496
Ausaf Ul Islam <165744535+Ausaf-ul-Islam@users.noreply.github.com>
2024-12-07 22:56:23 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex text-center h-screen justify-center flex-col items-center ">
+      <h1>Hello World</h1>
+      <h1>Hacking kese karte hain Hacking</h1>
+    </main>
+  );
+}
